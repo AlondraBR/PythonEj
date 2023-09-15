358a88367db76f913b9af9f8ba5aeab19ae43146
AlondraBR <141065060+AlondraBR@users.noreply.github.com>
2023-09-14 22:37:54 -0600

    Add files via upload

---
+++ ejercicio.py
@@ -0,0 +1,29 @@
+def condiciones():
+    # n1
+    # n2
+    # n3
+    int  print("Condiciones")
+    n1 = "80"
+    n2 = "90"
+    n3 = "100"
+    
+    if n1 >="80":
+                    print("3")
+        if n2 >= "90":
+            if n2 <="90"
+            print("4")
+            if n3 == "100":
+                print("5")
+            else:
+                print("4")
+        else:
+            print("3")
+    else:
+        print("no aprobado")
+
+condiciones()
+ 
+
+@AlondraBR
+ 
+
